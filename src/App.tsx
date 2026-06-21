6298e0e1c5abc440dd847be022a44599ce7968f6
Shihab Muhtasim <shihabmuhtasim@gmail.com>
2026-06-21 21:45:15 +0600

    Fix GitHub pages 404s by switching to HashRouter; update package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Intro from './components/Intro';
 import Navigation from './components/Navigation';
